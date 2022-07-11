3b3772e0333fa41703c157507dccd64ab49f8b36
saiak <saiak@LAPTOP-S59P6VT9>
2022-07-11 14:20:05 +0530

    Add project files.

---
+++ Models/Assignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.Models
+{
+    public class Assignment
+    {
+        [Key]
+
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Employeedesignation { get; set; }
+        public DateTime EmpDoj { get; set; }
+
+
+
+    }
+}
